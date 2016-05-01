df08fdafa0433002c309154947de1d444ff9fdc2
sunzhuyang <sunzhuyang3@163.com>
2016-05-01 21:56:22 +0800

    new

---
+++ src/main/java/com/github/szy8/wxapi/api/menu/WeChatMenuApi.java
@@ -0,0 +1,7 @@
+package com.github.szy8.wxapi.api.menu;
+
+/**
+ * Created by xiaozhu on 16/5/1.
+ */
+public interface WeChatMenuApi {
+}
---
+++ src/main/java/com/github/szy8/wxapi/api/menu/impl/WeChatMenuApiImpl.java
@@ -0,0 +1,7 @@
+package com.github.szy8.wxapi.api.menu.impl;
+
+/**
+ * Created by xiaozhu on 16/5/1.
+ */
+public class WeChatMenuApiImpl {
+}
